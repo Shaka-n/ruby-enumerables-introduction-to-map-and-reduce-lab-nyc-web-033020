6ff5328be32cabb0df6fb7e99bb6c073a8a94670
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-28 18:07:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,7 +41,7 @@ end
 
 def reduce_to_total(source_array, starting_point)
   index = 0
-  total = 0 
+  total = 0 + starting_point
   while index < source_array.length do
     total += source_array[index]
     index += 1
